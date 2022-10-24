1015d4baf3078af9a795c1a5b02fbd656f68c1cb
ZidniGanz <zidniganz@yahoo.com>
2022-10-24 22:47:04 +0700

    mmk

---
+++ config.js
@@ -0,0 +1,80 @@
+/** 
+
+Yg Mau Sv Nomerku?
+
+wa.me/6285215511208
+
+Minimal Kasih Cr kalo mau reupload😡
+Create By Dika Ardnt.
+Recode By Naze & ( Zaini )
+
+Kalau Sampai Ketemu Lagi Creator Bot
+Yg Reupload Tanpa Kasih Cr 
+Sc Akan Saya Encode
+
+Base : Hisoka
+Recode : Naze & ( Zaini )
+
+**/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6285215511208']
+global.ownernomer = "6285215511208"
+global.premium = ['6285215511208']
+global.packname = 'Sticker By'
+global.author = 'Xyzroth'
+global.sessionName = 'natsu'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: '✅Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+global.thumb = fs.readFileSync('./media/image/naze.jpg')
+global.faall = fs.readFileSync('./media/image/fake.jpg')
+global.mygit = 'https://github.com/nazedev'
+global.myyt = 'https://youtube.com/channel/UC7dcYh3bx2TzYIo2aoTFRYQ'
+global.myytv = 'https://youtu.be/wVofb2oVH5E'
+global.mygc = "https://chat.whatsapp.com/KG7WsBtayeABzQK3FahzMX"
+global.botname = 'Natsu Botz'
+global.akulaku = 'Bot By Zaini'
+global.ytname = 'NGAPAIN'
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
